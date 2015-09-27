8d628a943276c4464921976b98e45faae21cc6b1
Izaak Alpert <izaak.alpert@shopify.com>
2015-09-27 23:29:10 +0000

    do the addition

--- roman_numeral_addition.go
+++ roman_numeral_addition.go
@@ -1,33 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var numberMaps = map[string]int{
-  "M" : 1000,
+	"M": 1000,
 	"C": 100,
 	"X": 10,
 	"V": 5,
 	"I": 1,
 }
 
-var numberArray = []map[string]int {
-  {"M": 1000},
-  {"C": 100},
-  {"X": 10},
-  {"V": 5},
-  {"I": 1},
+var numberArray = []map[string]int{
+	{"M": 1000},
+	{"C": 100},
+	{"X": 10},
+	{"V": 5},
+	{"I": 1},
 }
-
 
 func main() {
 	fmt.Printf("foo")
 	input := "I+I"
 	fmt.Printf("input: %s\n", input)
-	fmt.Printf("output: %d\n", AddRoman("I", "I"))
+	fmt.Printf("output: %s\n", AddRoman("I", "I"))
 }
 
-func AddRoman(s1 string, s2 string) int {
-	return parseNum(s1) + parseNum(s2)
+func AddRoman(s1 string, s2 string) string {
+	return toRoman(parseNum(s1) + parseNum(s2))
 }
 
 func parseNum(s string) int {
@@ -56,18 +57,25 @@ func parseNum(s string) int {
 }
 
 func toRoman(input int) string {
- fmt.Printf("input:%d\n===============\n", input)
-  var store string
-  remainder := input
-  for i := 0; i < len(numberArray); i++ {
-    for numeral, value := range numberArray[i] {
-      for remainder >= value {
-        remainder -= value
-        store += numeral
-      }
-      fmt.Printf("remainder:%d, numeral:%s, value:%d, store:%s\n", remainder, numeral, value, store)
-    }
-  }
-  return store
+	var store string
+	remainder := input
+	for i := 0; i < len(numberArray); i++ {
+		for numeral, value := range numberArray[i] {
+			test := value
+			if value > 5 {
+				test = value - 2
+			} else if value == 5 {
+				test = value - 1
+			}
+			for remainder >= test {
+				remainder -= value
+				store += numeral
+			}
+			for remainder < 0 && remainder >= -2 {
+				remainder += 1
+				store = "I" + store
+			}
+		}
+	}
+	return store
 }
-
--- roman_numeral_addition_test.go
+++ roman_numeral_addition_test.go
@@ -5,29 +5,49 @@ import (
 )
 
 var conversionTests = map[string]int{
-    "IX":  9,
-    "IIX": 8,
-    "XII": 12,
-    "IIIX": 7,
-    "IVX": 4,
-    "VVX": 0,
-    "VIX": 4,
+	"IX":   9,
+	"IIX":  8,
+	"XII":  12,
+	"VII":  7,
+	"IV":   4,
+	"":     0,
+	"IIMM": 1998,
+	"III":  3,
 }
 
 func TestParseNum(t *testing.T) {
 	for numeral, value := range conversionTests {
-    result := parseNum(numeral)
-    if result != value {
-      t.Errorf("got unexpected value for roman numeral: %s, expected: %d, got %d", numeral, value, result)
-    }
-  }
+		result := parseNum(numeral)
+		if result != value {
+			t.Errorf("got unexpected value for roman numeral: %s, expected: %d, got %d", numeral, value, result)
+		}
+	}
 }
 
 func TestToRoman(t *testing.T) {
-for numeral, value := range conversionTests {
-    result := toRoman(value)
-    if result != numeral {
-      t.Errorf("got unexpected value for number: %d, expected: %s, got %s", value, numeral, result)
-    }
-  }
+	for numeral, value := range conversionTests {
+		result := toRoman(value)
+		if result != numeral {
+			t.Errorf("got unexpected value for number: %d, expected: %s, got %s", value, numeral, result)
+		}
+	}
+}
+
+func TestAddRoman(t *testing.T) {
+	tests := [][]string{
+		{"I", "I", "II"},
+		{"II", "I", "III"},
+		{"IV", "I", "V"},
+		{"V", "I", "VI"},
+		{"VI", "I", "VII"},
+		{"I", "IX", "X"},
+		{"X", "IV", "IXV"},
+	}
+
+	for test := 0; test < len(tests); test++ {
+		result := AddRoman(tests[test][0], tests[test][1])
+		if result != tests[test][2] {
+			t.Errorf("%s + %s = %s, got: %s", tests[test][0], tests[test][1], tests[test][2], result)
+		}
+	}
 }
